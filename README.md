1d85c4f44dbb6d8fd883c5df396d8d93031d2aaa
Sergiy Antonyuk <s.p.antonyuk@gmail.com>
2024-08-01 18:43:37 +0200

    final

---
+++ README.md
@@ -0,0 +1,28 @@
+## Simple Real Time Chat Application
+
+###  Chat Application with React (Vite), TailwindCSS, Node, Socket.io & Express 
+
+---
+
+![](demo.gif)
+
+
+
+```bash
+/client
+npm run dev
+
+/server
+npm run dev
+```
+
+Open [http://localhost:5173/](http://localhost:5173/) in your browser to view the project.
+
+
+---
+
+##### Contact with me:
+
+[<img alt="webDev | LinkedIn" src="https://img.shields.io/badge/linkedin-0077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" />][linkedin]
+
+[linkedin]: https://www.linkedin.com/in/sergiy-antonyuk/
---
+++ server/index.js
@@ -0,0 +1,35 @@
+// This script creates a Node.js application that:
+
+// - Establishes a web server using Express.js.
+// - Initializes a Socket.IO server to enable real-time communication.
+// - Handles client connections and disconnections.
+// - Implements a simple chat functionality by broadcasting messages received from clients to all connected clients.
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
